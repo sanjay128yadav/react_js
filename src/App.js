78d15d18d1f9f850940ca8674aab60960d135dc7
sanjay128yadav <sanjay128yadav@gmail.com>
2024-07-23 22:52:03 +0530

    23-July-2024

--- src/App.js
+++ src/App.js
@@ -1,114 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {Component, useState} from "react";
-//import { Users } from './Users'
-import Users from './Users';
-import { UserClass, TeacherClass } from './UsersClass';
-import Myuser from './Myuser.jsx';
-import Counter from './Counter.jsx';
-import ClassCounter from './ClassCounter.jsx';
-import UsersCardProps from './UsersCardProps.jsx';
-import UsesProps from './UsersProps.jsx';
-import UsersListClassProps from './UsersListClassProps.jsx';
+import React, {useState} from 'react'
+import "./App.css";
 
-
-//import { TeacherClass } from './UsersClass';
-
-function App() {
-
-  const usersData = [
-    { name: 'Mansh Kumar', age: 24, email: 'manish@gmail.com' },
-    { name: 'Sonu Kumar', age: 24, email: 'manish@gmail.com' }
-  ];
-
-  function Common() {
-    return(
-      <div>This Common Component Called from within another component </div>
-    ) 
-  }
-
-  let message = "Hi good morning";
-  const demo = () => {
-        alert("Hi how are you?");
-        alert(message);
-  }
-
-  let [data, setData] = useState("React Js"); 
-
-  const updateData = () => {
-    setData("Python with React Js");
-    alert(data);
-  }
-
-  console.log("Render...........");
-
-  // Example 3
-  const [name, setName] = useState("Old Props Data");
-
-  // Use propes with Class Component
-
-  
-
-  
+export default function App() {
+  const [status, setSataus] = useState(true);
   return (
+    <div className='App'>
     <>
-    <div className="App">
-      <h1>{data}</h1>     
+      <h1>Show Hide Toggle</h1>
+      {status ? <div className='status'>Content</div>: null}  
 
-      <Users />
-      <UserClass />
-      <TeacherClass />
-      <MonitorClass />
-      <Myuser />
-      {Common()}
-      <Common />
-      <button onClick={demo}>Click me</button><br/>
-      <button onClick={()=> alert("Hi I am Sanjay Yadav")}>Click me using Fat Arrow Function</button><br/>
-      <button onClick={()=> demo()}>Click here</button><br/>
-      <button onClick={updateData}>Update Data</button>
-      <br />
-      <button onClick={()=> setName("Updated Props Data")}>Update Props Data</button>
-      <br />
-      <UsesProps name={name}/>
-      <br />
-      <UsersListClassProps name="Sonu Kumar"  email="sonu@gmail.com" />      
-      <br />
-      <Counter />
+      {/* Example 1 Show & Hide*/}    
+      <button onClick={()=>setSataus(false)}>Hide</button>
+      <button onClick={()=>setSataus(true)}>Show</button>
 
-      <br /><br />
-      <ClassCounter />
-
-      {/* Example 2 */}
-
-      {
-        usersData.map((user, index) => (
-          <UsersCardProps
-            key = {index}
-            name = {user.name}
-            age = {user.age}
-            email = {user.email}
-          />
-        ))
-      }
-            
+      {/* Example 2 Toggle*/} 
+      <button onClick={()=>setSataus(!status)}>Toggle</button>
+    </>  
     </div>
-    </>
-  );
+  )
 }
-
-class MonitorClass extends Component {
-  render() {
-      return(
-          <div>Monitor Class Component</div>
-      )
-  }
-}
-
-// Inside  the App.js
-/*
-function Users() {
-  return <h1>Js Component</h1>
-}
-  */
-
-export default App;
