d0ea8d2fcc7de100f6a407b3496ec058ec9d33a0
sanjay128yadav <sanjay128yadav@gmail.com>
2024-08-01 23:00:45 +0530

    01Aug-2024

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React, {useState} from 'react'
+import React, { Component } from 'react'
 import './App.css'
-import RenderMethod from './RenderMethod'
-import RenderMethodSetState from './RenderMethodSetState'
 
-export default function App() {
-  const [name, setName] = useState("Samir")
-  return (
-    <>
-    <div className='App'>React Render Method</div>
-
-    <RenderMethod name = {name} />
-
-    <button onClick={() =>setName("Sanjaty Yadav")}>Update Name</button>
-
-    <RenderMethodSetState />
-    </>
-    
-  )
+export default class App extends Component {
+  constructor(){
+      super();
+      this.state = {
+        count:0,
+      }
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('ShouldComponentUpdate method called', this.state.count, nextState);
+    if(nextState.count <5) {
+      return true;
+    }
+    return false;
+  }
+  render() {
+    console.log("Render called");
+    return (
+      <>
+      <div className='App'>Should Component Update</div>
+      <h2>Count: {this.state.count}</h2>
+      <button onClick={()=> this.setState({count:this.state.count +1})}>Update State</button>
+      </>
+    )
+  }
 }
