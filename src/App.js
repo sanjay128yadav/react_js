8201a9317c4f2584b1a0326bbe2ceeb8e3e3d09c
sanjay128yadav <sanjay128yadav@gmail.com>
2024-08-04 01:23:25 +0530

    RJ #03082024

--- src/App.js
+++ src/App.js
@@ -1,68 +1,44 @@
 import React from 'react'
 import './App.css'
 
+import Table from 'react-bootstrap/Table';
+
 export default function App() {
   // Example 1
 
-  const names = ["Samir", "Anil", "Navneet", "Saurabh"];
-
-  //Example 2 Array of objects
-
-  const products = [
-    {
-      id: 1,
-      name: "Product 1",
-      price: 100
-    },
-    {
-      id: 2,
-      name: "Product 2",
-      price: 200
-    },
-    {
-      id: 3,
-      name: "Product 3",
-      price: 300
-    }
+  const dataTable = [
+    {id:1, name:"Madan", age:22, location:"Noida"},
+    {id:2, name:"Ramesh",age:23, location:"Ghaziabad"},
+    {id:3, name:"Sunil", age:24, location:"Kanpur"},
+    {id:4, name:"Rahul", age:25, location:"Lucknow"},
+    {id:5, name:"Mohan", age:26, location:"Varansi"},
   ]
-
-  // Example 3 Return map method
-
-  const fruits = ["Apple", "Banana", "Orange", "Govava", "Mango"];
-
-  // Using map to transform the array of fruits into an array of react element
-
-  const fruitsIteam = fruits.map((fruit, index)=> <li key={index}>{fruit}</li>)
-
   return (
     <>
     <div className='App'>
-      <h2>Array with Map Function</h2>
-
-      {/* Example 1 */}
-
-      <ul>
-        {
-          names.map((name, index)=>(
-            <li key={index}>{name}</li>
-          ))
-        }
-      </ul>
-
-      {/* Example 2 */}
-
-      <ul>
-        {
-          products.map((product)=>(
-            <li key={product.id}>{product.name}-{product.price}$</li>
-          ))
-        }
-      </ul>
-
-        {/* Example 3 */}
-
-        <ul>{fruitsIteam}</ul>
-
+      <h2>Bootstrap Table In React </h2>
+      <div>
+        <Table striped bordered hover size="sm" variant="dark">
+            <thead>
+              <tr> 
+                <th>ID</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Location</th>
+              </tr>
+            </thead>
+            <tbody>
+              {dataTable.map((data)=>(
+                <tr key={data.id}>
+                  <td>{data.id}</td>
+                  <td>{data.name}</td>
+                  <td>{data.age}</td>
+                  <td>{data.location}</td>
+                </tr>
+              ))}
+            </tbody>
+        </Table>
+      </div>
     </div>
     </>
   )
