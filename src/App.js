1710aad0e0fc66b5c00b76c7ae7a2adaf94cfcf6
sanjay128yadav <sanjay128yadav@gmail.com>
2024-08-03 23:50:53 +0530

    RJ #03082024

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React from 'react'
 import './App.css'
 
+// Bootstrap 5
+//import Button from 'react-bootstrap/Button';
+
+//import Alert from 'react-bootstrap/Alert';
+
+import {Button, Alert} from 'react-bootstrap';
+
 export default function App() {
   return (
     <>
     <div className='App'>
-      <h2>Install Bootstrap In React</h2>
+      <h2>Install Bootstrap In React Js</h2>
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
+
+      {[
+        'primary',
+        'secondary',
+        'success',
+        'danger',
+        'warning',
+        'info',
+        'light',
+        'dark',
+      ].map((variant) => (
+        <Alert key={variant} variant={variant}>
+          This is a {variant} alert—check it out!
+        </Alert>
+      ))}
+
+      <Alert variant='success'>This is single static alert</Alert>
     </div>
     </>
   )
