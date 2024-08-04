fc79834cacd9318ae4812ca77ec97c0b7c32113f
sanjay128yadav <sanjay128yadav@gmail.com>
2024-08-04 23:58:33 +0530

    RJ #04082024

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React, {useState} from 'react'
+import React, {useState, useMemo} from 'react'
 import './App.css'
-import ChildComponent from './ChildComponent';
 
 export default function App() {
 
   // Example 1
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
   const [count, setCount] = useState(0);
-  console.log("Parent");
+
+  /* const sum = ()=> {
+    console.log("Calculating Sum...!");
+    return number1 + number2;
+  } */
+
+    const sum = useMemo(()=> {
+      console.log("Calculating Sum...!");
+      return number1 + number2;
+    }, [number1, number2])  
+
   return (
     <>
       <div className='App'>
-        <h2>Memo In React.</h2>
+        <h2>React useMemo Hook</h2>
+        {/* Example 1 */}
 
-        <ChildComponent />
+        <input type='number' value={number1} onChange={(e)=>setNumber1(Number(e.target.value))} />
+        <input type='number' value={number2} onChange={(e)=>setNumber2(Number(e.target.value))} />
 
-        <h6>Counter: {count}</h6>
-        <button onClick={()=>setCount(count+1)}>Update</button>
+
+        {/* Example 1
+         <p>Sum: {sum()}</p> */}
+
+        {/* Example 2 */}
+
+        <p>Sum Using useMemo: {sum}</p> 
+
+        {/* Example 1 */}
+
+        <h4>Count Number: {count}</h4>
+        <button onClick={()=>setCount(count + 1)}>Count++</button>
       </div>
     </>
   )
