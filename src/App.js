f02c0832764b25df78ad0de240e078299dad6e00
sanjay128yadav <sanjay128yadav@gmail.com>
2024-08-04 18:13:48 +0530

    RJ #04082024

--- src/App.js
+++ src/App.js
@@ -1,69 +1,39 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import './App.css'
 
 export default function App() {
-  // Example 1
-
-  const names = ["Samir", "Anil", "Navneet", "Saurabh"];
-
-  //Example 2 Array of objects
-
-  const products = [
-    {
-      id: 1,
-      name: "Product 1",
-      price: 100
-    },
-    {
-      id: 2,
-      name: "Product 2",
-      price: 200
-    },
-    {
-      id: 3,
-      name: "Product 3",
-      price: 300
-    }
-  ]
-
-  // Example 3 Return map method
-
-  const fruits = ["Apple", "Banana", "Orange", "Govava", "Mango"];
-
-  // Using map to transform the array of fruits into an array of react element
-
-  const fruitsIteam = fruits.map((fruit, index)=> <li key={index}>{fruit}</li>)
-
   return (
-    <>
-    <div className='App'>
-      <h2>Array with Map Function</h2>
 
-      {/* Example 1 */}
+      /* 
+      <React.Fragment>
+        <h2>Fragment In React</h2>
+        <h2>Using React.Fragment</h2>  
+      </React.Fragment>
+      
+      
+      
+      <Fragment>
+          <h2>Using Only Fragment</h2>  
+          <h2>Using Only Fragment</h2>  
+      </Fragment>
 
-      <ul>
-        {
-          names.map((name, index)=>(
-            <li key={index}>{name}</li>
-          ))
-        }
-      </ul>
+      
 
-      {/* Example 2 */}
+      
+      <div className='App'>
+        <h2>Using Only div</h2>  
+        <h2>Using Only div</h2> 
+      </div>
 
-      <ul>
-        {
-          products.map((product)=>(
-            <li key={product.id}>{product.name}-{product.price}$</li>
-          ))
-        }
-      </ul>
+      */
 
-        {/* Example 3 */}
-
-        <ul>{fruitsIteam}</ul>
-
-    </div>
-    </>
+      <>
+        <div>
+          <h2>Using Only empty tag</h2> 
+          <h2>Using Only empty tag</h2> 
+        </div>
+      </>
+    
+    
   )
 }
