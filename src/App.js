c8e311b2b0ef08885b021b11262a96016c0fa064
sanjay128yadav <sanjay128yadav@gmail.com>
2024-08-05 23:31:29 +0530

    RJ 05082024

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
-import React, { Component, createRef } from 'react'
+import React, {useRef} from 'react'
 import './App.css'
 
-export default class App extends Component {
-
- constructor() {
-  super();
-  this.myInputRef = createRef();
- } 
+export default function App() {
+  // Example 1
+  const inputRef = useRef(null);
 
  // Example 2
 
- /* componentDidMount() {
-  this.myInputRef.current.value = "Sanjay Yadav";
-  console.log(this.myInputRef.current.value); 
- } */
-
-  // Example 2
-
-  handleClick = () => {
-    this.myInputRef.current.focus();
-    this.myInputRef.current.style.background ="green";
-    this.myInputRef.current.style.color ="White";
-    console.log(this.myInputRef.current.value);
-  }
+ const handleButtonClick = () =>{
+    const inputValue = inputRef.current.value;
+    alert(`Input Value ${inputValue}`);
+    console.log(`Input Value ${inputValue}`);
+ }
 
 
-  render() {
-    /* console.log(this.myInputRef); */
-    return (
-      <>
-        <div className='App'>
-          <h2>Class Ref In React</h2>
-          <input type='text' ref={this.myInputRef} />
-          <button onClick={this.handleClick}>Focus Input</button>
-        </div>
-      </>
-    )
-  }
+  return (
+    <>
+      <div className='App'>
+        <h2>Function based useRef Hook</h2>
+        <input type='text' ref={inputRef} />
+        <button onClick={handleButtonClick}>Get Input Value</button>
+      </div>
+    </>
+  )
 }
