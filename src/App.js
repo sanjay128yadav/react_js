43e86109e39aba2ced3fadd3fcf06ac72465f1a7
sanjay128yadav <sanjay128yadav@gmail.com>
2024-08-14 00:34:54 +0530

    RJ #13082024

--- src/App.js
+++ src/App.js
@@ -1,42 +1,25 @@
-import React, {useRef} from 'react'
+import React, {useSyncExternalStore} from 'react'
 import './App.css'
-import ChildComponent from './ChildComponent'
 
 export default function App() {
- 
+
   // Example 1
-  const inputRef = useRef(null);
-
-  const updateInput = ()=> {
-    inputRef.current.value = 10000;
-    inputRef.current.style.color = 'white'
-    inputRef.current.style.background = 'blue'
-    inputRef.current.focus();
-  }
-
-  // Example 2
-
-  const childRef = useRef(null);
-
-  const handleButtonClick = ()=>{
-    if(childRef.current) {
-
-      childRef.current.handleButtonClick();
-
-    }
-  }
+  const  width = useSyncExternalStore(
+    (listner)=>{
+      window.addEventListener("resize", listner);
+      return() => {
+        window.removeEventListener("resize", listner);
+      }
+    }, 
+    ()=>window.innerWidth
+  );
 
   return (
     <>
       <div className='App'>
-          <h2>forward Ref in React</h2>
-          {/* <ChildComponent ref={inputRef} />
-          <button onClick={updateInput}>Update Input Value</button> */}
 
-          {/* Example 2 */}
-          <ChildComponent ref={childRef} />
-
-          <button onClick={handleButtonClick}>Click Button from Parent</button>
+          <h4>useSyncExternalStore React Hook</h4>
+          <p>Width: {width}</p>
       </div>
     </>
   )
