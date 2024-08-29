d080b7e326a88250f062775edaf2f2c86f4fdc60
sanjay128yadav <sanjay128yadav@gmail.com>
2024-08-29 23:22:50 +0530

    RJ #29082024

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./App.css";
 export default function App() {
   const [status, setSataus] = useState(true);
   return (
-    <div className='App'>
     <>
+    <div className='App'>    
       <h1>Show Hide Toggle</h1>
       {status ? <div className='status'>Content</div>: null}  
 
@@ -14,8 +14,8 @@ export default function App() {
       <button onClick={()=>setSataus(true)}>Show</button>
 
       {/* Example 2 Toggle*/} 
-      <button onClick={()=>setSataus(!status)}>Toggle</button>
-    </>  
+      <button onClick={()=>setSataus(!status)}>Toggle</button>      
     </div>
+    </>
   )
 }
